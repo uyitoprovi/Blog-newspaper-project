e5dc595674e97676c1010ba491853f875d6f821a
uyitoprovi <uyitoprovi@gmail.com>
2024-08-16 12:10:54 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from django.http import Http404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+
+
+def post_list(request):
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+
+    return render(request, "blog/post/detail.html", {"post": post})
